33c6b18f88477ca69aa220a6d18db0d762ea1578
donowhy <donowhy11@gmail.com>
2016-07-30 23:29:34 -0700

    works on its own instance in beanstalk

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^signout$', views.signout, name='signout'),
-    url(r'^signin$', views.signin, name='signin'),
-    url(r'^headerSignIn/$', views.headerSignIn, name='headerSignIn'),
-    url(r'^compareUser/', views.compareUser, name='compareUser'),
-    url(r'^genRoutine/', views.genRoutine, name='genRoutine')
+    url(r'^incomingPOSTAndroid$', views.incomingPOSTAndroid, name='incomingPOSTAndroid'),
+
 ]
